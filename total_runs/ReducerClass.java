05b89f2613c583a7be5b03a7432be2fab8d7d043
Arunima Barik <arunimabarik75@gmail.com>
2023-11-07 14:27:46 +0530

    Total runs in the match - Mapper Reducer

---
+++ total_runs/ReducerClass.java
@@ -0,0 +1,21 @@
+package total_runs;
+
+import org.apache.hadoop.mapreduce.*;
+import java.io.IOException;
+import org.apache.hadoop.io.*;
+
+public class ReducerClass extends Reducer<Text,IntWritable,Text,IntWritable>{
+	@Override
+	protected void reduce(Text key, Iterable<IntWritable> values,
+			Reducer<Text, IntWritable, Text, IntWritable>.Context op) throws IOException, InterruptedException {
+		// TODO Auto-generated method stub
+		int sum =0;
+		for(IntWritable data : values) {
+			int b = data.get();
+			sum = sum + b;
+		}
+		Text key1 = key;
+		IntWritable ov = new IntWritable(sum);
+		op.write(key1, ov);
+	}
+}
