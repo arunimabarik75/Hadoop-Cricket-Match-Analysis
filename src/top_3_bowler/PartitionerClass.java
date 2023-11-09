de9f73966a4f0a59e4bc602701b906ad83b5f9ea
Gaurav Golchha <gauravjain78200@gmail.com>
2023-11-09 09:49:07 +0530

    updated top 3 bowler partitioner class

--- src/top_3_bowler/PartitionerClass.java
+++ src/top_3_bowler/PartitionerClass.java
@@ -4,12 +4,22 @@ import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.*;
 
 public class PartitionerClass extends Partitioner<Text,Text>{
+	
+	String team1;
+
+	PartitionerClass(){
+		this.team1 = "";
+	}
+	
 	@Override
 	public int getPartition(Text arg0, Text arg1, int arg2) {
 		
 		String key = arg0.toString();
 		String [] k = key.split("[,]");
-		if(k[1].equals("Australia")) {
+		if(this.team1==""){
+			this.team1 = k[1];
+		}
+		if(k[1].equals(this.team1)) {
 			return 0;
 		}
 		else {
